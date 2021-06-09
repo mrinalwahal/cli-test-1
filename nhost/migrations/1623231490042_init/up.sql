582cc1d58f9b5573033b824d265d1357ac5c6689
mrinal wahal <mrinal_w@rocketmail.com>
2021-06-09 15:11:27 +0530

    trying fresh

--- nhost/migrations/1623231490042_init/up.sql
+++ nhost/migrations/1623231490042_init/up.sql
@@ -38,14 +38,11 @@ CREATE TABLE IF NOT EXISTS public.users (
     display_name text,
     avatar_url text
 );
-ALTER TABLE ONLY public.test
-    ADD CONSTRAINT test_pkey PRIMARY KEY (id);
-
+SELECT create_constraint_if_not_exists('public.test', 'test_pkey', 'PRIMARY KEY (id);');
 SELECT create_constraint_if_not_exists('public.users', 'users_pkey', 'PRIMARY KEY (id);');
-
 DROP TRIGGER IF EXISTS set_public_users_updated_at ON public.users;
-	CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
-	
+CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+
 
 INSERT INTO auth.roles (role)
     VALUES ('user'), ('me'), ('anonymous') ON CONFLICT DO NOTHING;
